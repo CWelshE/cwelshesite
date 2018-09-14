86d4234c7ab218f68ad875d4d6bd64125aa7fd68
Cody Welsh <codyw@protonmail.com>
2018-09-13 21:56:05 -0700

    chore(Index): Add index intro text, missing const
    Adds some relevant text to the first page, and adds a file that's
    missing from the repository.

---
+++ src/constants/text.js
@@ -0,0 +1,15 @@
+// Assorted values for styles
+const text = {
+  default: {
+    typeface: 'Roboto Slab',
+  },
+  header: {
+    size: '5.5rem',
+    margins: '25px 0 30px 0',
+  },
+  colors: {
+    primary: '#e0dfd5',
+  },
+};
+
+export default text;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi. I&apos;m Cody.</h1>
+    <p>
+      My main passion is software engineering. Right now, I work on websites,
+      experiments, procedural art, and all manner of applications.
+    </p>
+    <p>
+      My tools of choice include (but are certainly not limited to): (P)React,
+      Redux (when necessary), PostgreSQL, Webpack, moral support from my cat, an
+      obsession with Vim, and whatever &quot;gets the job done.&quot;
+    </p>
   </Layout>
 );
 
