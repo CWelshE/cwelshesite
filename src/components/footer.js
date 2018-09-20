3b201bf6ffbbca73ece005d9851301cdc2e4d555
Cody Welsh <codyw@protonmail.com>
2018-09-19 18:41:22 -0700

    styles(Footer): Add better styles
    Now, instead of looking like nothing at all, this change gives the
    footer some presence. :)

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
+import text from '../constants/text';
 
 const StyledFooter = styled('footer')`
   display: flex;
   width: 100%;
   height: 100px;
+  font-size: smaller;
+  background: rgba(0, 0, 0, 0.2);
 `;
 
 // A grid with 3 equal columns and two equal rows
 const section = css`
+  font-family: ${text.default.typeface};
+  color: ${text.colors.primary};
   display: grid;
   width: 100%;
-  grid-template-columns: 50% 50%;
+  grid-template-columns: 33% 33% 33%;
+  > span {
+    padding-top: 2rem;
+  }
   > span:first-of-type {
-    border: 1px solid red;
+    vertical-align: middle;
+    padding-left: calc(13vw - 35px);
   }
 `;
 
@@ -22,7 +31,11 @@ const FooterSection = (
     <span>
       Copyright &copy; Cody Welsh, 2018 and onwards. All rights reserved.
     </span>
-    <span>If nothing else, please try to be kind to other people.</span>
+    <span />
+    <span>
+      If nothing else, please try to be kind to other people. Everyone is
+      fighting their own battle in life.
+    </span>
   </div>
 );
 
