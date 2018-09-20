d050b9e64e61a02d65b30541a74644920019f2fa
Cody Welsh <codyw@protonmail.com>
2018-09-19 18:45:03 -0700

    fix(Styles): Breakpoints
    Adds a missing array export for breakpoints sizes.

---
+++ src/constants/breakpoints.js
@@ -0,0 +1,6 @@
+// An array of breakpoints, in px.
+// Font sizes will attempt to calc between the smallest and largest
+// values in the array.
+const breakpoints = [320, 960];
+
+export default breakpoints;
