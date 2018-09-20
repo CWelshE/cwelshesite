0baaf7129d96f5bcc46ab16f65cd00efd9b4b3d9
Cody Welsh <codyw@protonmail.com>
2018-09-19 18:42:48 -0700

    feat(Styles): CSS Lock Calculation / Header
    Gives some dynamic calculations to the header's font size, based on the
    user's own font size and the viewport width (taking into account the
    possibility of user changes).

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,10 @@
 // The Header component is wrapped by `layout.js`.
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'react-emotion';
 
 import text from '../constants/text';
 
-const StyledHeader = styled('div')`
-  display: flex;
-  color: ${text.colors.primary};
-  font-size: ${text.header.size};
-  margin: ${text.header.margins};
-  border-bottom: 3px solid;
-  line-height: 80px;
-  max-width: 550px;
-`;
+const StyledHeader = text.header;
 
 const Header = props => {
   return <StyledHeader>{props.text}</StyledHeader>;
--- src/constants/text.js
+++ src/constants/text.js
@@ -1,12 +1,40 @@
-// Assorted values for styles
+// Assorted values for text styles
+import styled, { css } from 'react-emotion';
+import mq from './breakpoints';
+
+// See the following for excellent CSS lock math:
+// https://fvsch.com/css-locks/#toc-1-b
+const generateQueries = (fontDefault, fontIncrease, breakMin, breakMax) => {
+  const fontSlope = fontIncrease / (breakMax - breakMin);
+  return `font-size: ${fontDefault}rem;
+
+    @media screen and (min-width: ${breakMin}px) {
+      font-size: calc(${fontDefault}rem + ${fontSlope * 100}vw + ${0 -
+    fontSlope * breakMin}px);
+    }
+
+    @media screen and (min-width: ${breakMax}px) {
+      font-size: calc(${fontDefault}rem + ${fontIncrease}px);
+  }`;
+};
+
+// Object containing common text styles
 const text = {
   default: {
     typeface: 'Roboto Slab',
   },
-  header: {
-    size: '5.5rem',
-    margins: '25px 0 30px 0',
-  },
+  header: styled('h1')`
+    display: flex;
+    margin: 10px 0;
+    max-width: 550px;
+
+    ${generateQueries(2.5, 30, mq[0], mq[1])};
+  `,
+  subheader: css`
+    margin: 0;
+
+    ${generateQueries(1.75, 20, mq[0], mq[1])};
+  `,
   colors: {
     primary: '#e0dfd5',
   },
