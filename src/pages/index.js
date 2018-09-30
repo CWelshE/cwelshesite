b84b6bef8bb4aeb8c4ee62e5c2d99cf822b2e779
Cody Welsh <codyw@protonmail.com>
2018-09-29 20:58:58 -0700

    chore(Index): Add more copy
    It's my site, so I guess I should add more information about myself.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,29 @@ const IndexPage = () => (
       Redux (when necessary), PostgreSQL, Webpack, moral support from my cat, an
       obsession with Vim, and whatever &quot;gets the job done.&quot;
     </p>
+    <p>
+      I live by these principles, and they serve me well in software
+      development, friendships, and also just life in general, too:
+    </p>
+    <ul>
+      <li>Leave things better than they were;</li>
+      <li>Exercise critical thinking as much as possible;</li>
+      <li>Be mindful of others, and their feelings;</li>
+      <li>Have more patience than needed;</li>
+      <li>Greet conflicting viewpoints with empathy;</li>
+      <li>Communicate more than you perceive you need to;</li>
+      <li>Improve skills and learn new things every day;</li>
+      <li>
+        Imagine the world as a better place, and make your own impact on the
+        world to reach it.
+      </li>
+    </ul>
+    <p>Life is short, and no one person can learn everything, but I can try!</p>
+    <p>
+      I hope you can find what you&apos;re looking for in my tiny slice of the
+      Internet. If not, you can always send me a message. Collaboration is a
+      welcome inquiry.
+    </p>
   </Layout>
 );
 
