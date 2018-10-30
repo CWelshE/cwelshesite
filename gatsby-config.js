1a611f0a329a9f911db7e8738c30b193b69634bd
Cody Welsh <codyw@protonmail.com>
2018-10-30 15:23:37 -0700

    feat(Config): Parse Markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: 'codywel.sh',
+    title: './codywel.sh',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/src/pages/blog`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src`,
+      },
+    },
+  ],
 };
