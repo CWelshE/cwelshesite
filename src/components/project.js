8754c44d1d22976f74a9f0e168666bc1280c9432
Cody Welsh <codyw@protonmail.com>
2019-11-24 20:55:06 -0800

    feat(Projects): Add a first project
    Adds Codekite-js to the projects list. More to come!

--- src/components/project.js
+++ src/components/project.js
@@ -6,24 +6,33 @@ import text from '../constants/text';
 const projectStyles = css`
   background: rgba(0, 0, 0, 0.2);
   border-radius: 5px;
-  padding: 3rem;
+  padding: 1rem;
+  margin: 2rem 0;
   font-family: ${text.default.typeface}
   color: ${text.colors.primary};
 `;
 
 // Stateless "Project container".
 const Project = props => {
+  const handleClick = e => {
+    e.preventDefault();
+    window.location = props.linkTo;
+  };
+
   return (
-    <div css={projectStyles}>
+    <div css={projectStyles} onClick={handleClick}>
       <h3>{props.title}</h3>
+      <img src={props.projImg} />
       <p>{props.description}</p>
     </div>
   );
 };
 
 Project.propTypes = {
-  title: PropTypes.object.isRequired,
-  description: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  linkTo: PropTypes.string.isRequired,
+  projImg: PropTypes.string.isRequired,
 };
 
 export default Project;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,46 +1,31 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { css } from '@emotion/core';
 import Layout from '../components/layout';
-import PropTypes from 'prop-types';
 import Project from '../components/project';
 
-// TODO: Make `Project` component
-// TODO: Figure out how to retrieve a list of projects
-// TODO: Iterate on `Project` component list to form page
+import codekiteLogo from '../images/codekite-logo.svg';
+
+const projectsStyles = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
 const projects = () => {
   return (
     <Layout>
-      <div>
-        <h1>Stuff I&apos;ve Made</h1>
-        <Project title={'Test title'} description={'Test description'} />
+      <h1>Stuff I&apos;ve Made</h1>
+      <div css={projectsStyles}>
+        <Project
+          linkTo={'https://github.com/CWelshE/codekite-js'}
+          title={'Codekite JS Starter Kit'}
+          projImg={codekiteLogo}
+          description={
+            'An understandable starting point for those interested in the inner-workings of their JS projects.'
+          }
+        />
       </div>
     </Layout>
   );
 };
-
-projects.propTypes = {
-  data: PropTypes.object.isRequired,
-};
-
-export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          excerpt
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default projects;
